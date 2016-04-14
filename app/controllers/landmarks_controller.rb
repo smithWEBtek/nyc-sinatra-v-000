7b68c27f3e24b8b7eb2bc8b2b8a0d76be5bb3942
smithWEBtek <brad@smithwebtek.com>
2016-04-14 09:43:57 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -17,20 +17,20 @@ class LandmarksController < ApplicationController
   end
 
   get '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
+    @landmark = Landmark.find(params["id"])
     erb :'landmarks/show'
   end 
 
   get '/landmarks/:id/edit' do 
-    @landmark = landmark.find(:id)
+    @landmark = Landmark.find(params["id"])
     erb :'/landmarks/edit'
   end
 
   patch '/landmarks/:id' do 
-    @landmark = landmark.find_by_slug(params[:id])
-
+    @landmark = Landmark.find(params["id"])
+    @landmark.name = params["landmark"]["name"]
+    @landmark.year_completed = params["landmark"]["year_completed"]
     @landmark.save
     erb :'landmarks/show'
   end
-
 end
